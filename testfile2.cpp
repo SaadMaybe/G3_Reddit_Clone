aa5317ccb99a806c546b30f88f89ea55bfd18872
Tahha Khan <81804010+KGUY1@users.noreply.github.com>
2021-11-12 23:48:24 +0500

    commit to branch

---
+++ testfile2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hellwow";
+    cout << "IF GHENGHIS KHAN SO KHAN YOU"<<endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<"Don't forget to like, comment and subscribe.";
+    }
+    for (int i = 10; i > 0; i--)
+    {
+        cout<<i;
+    }
+}
