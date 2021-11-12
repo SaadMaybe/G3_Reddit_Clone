7196450103637d083fb4e0bca18f95225d5eb03a
Tahha Khan <81804010+KGUY1@users.noreply.github.com>
2021-11-12 23:26:33 +0500

    new change 2

--- testfile2.cpp
+++ testfile2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout<<"Hellwow";
-    cout << "If genghis khan, So khan you"<<endl;
+    cout << "If genghis khan, So khan you!"<<endl;
     for (int i = 0; i < 10; i++)
     {
         cout<<i;
