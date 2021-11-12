efe1df41831456734cdb29c37a72f4c38274f932
Tahha Khan <81804010+KGUY1@users.noreply.github.com>
2021-11-12 23:26:13 +0500

    newChange

--- testfile2.cpp
+++ testfile2.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
     cout<<"Hellwow";
-<<<<<<< HEAD
-    cout << "Genghis Khan died for your sins"<<endl;
-=======
-    cout << "Saad the great !!!"<<endl;
+    cout << "If genghis khan, So khan you"<<endl;
     for (int i = 0; i < 10; i++)
     {
-        cour<<i;
+        cout<<i;
     }
-    
->>>>>>> 5f074fd747722d3c3b6a96f14b6d06040aa9aec4
 }
