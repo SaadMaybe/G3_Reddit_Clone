7dededba6c9533608706f889faf9309c69985798
taimursalman <58456445+taimursalman@users.noreply.github.com>
2021-11-18 02:40:12 +0500

    backend teamwork for now
    sigh

--- main.py
+++ main.py
@@ -11,10 +11,55 @@ app.config['MYSQL_DB'] = 'reddit2'
 
 mysql = MySQL(app)
 
+def create_subreddit_case(username = "sad", subreddit_name= "woosh", description = "woosh brotha"):
+    cursor = mysql.connection.cursor()
+    
+    cursor.execute("Select username from reddit2.users where username=%s", (username,))
+    if(cursor.fetchone() == None):
+        print("User does not exist")
+        return False
+    
+    cursor.execute("Select subreddit_name from reddit2.subreddits where subreddit_name=%s", (subreddit_name,))
+    if(cursor.fetchone() != None):
+        print("Subreddit already exists")
+        return False
+    
+    #Insert into the subreddit table
+    cursor.execute("INSERT INTO reddit2.subreddits VALUES(%s, %s, %s)", (subreddit_name, description, username))
+    mysql.connection.commit()
+
+    #Insertion into the Joined table, to show that the user has created the subreddit
+    cursor.execute("INSERT INTO reddit2.joined VALUES(%s, %s. %s)", (username, subreddit_name, "Subreddit Owner"))
+    mysql.connection.commit()
+    
+    return True    
+    
+    
+def leave_subreddit_case(username ="sad", subreddit_name = "rip", ):
+    cursor = mysql.connection.cursor()
+    
+    cursor.execute("Select username from reddit2.users where username=%s", (username,))
+    if(cursor.fetchone() == None):
+        print("User does not exist")
+        return False
+    
+    cursor.execute("Select subreddit_name from reddit2.subreddits where subreddit_name=%s", (subreddit_name,))
+    if(cursor.fetchone() == None):
+        print("Subreddit does not exist")
+        return False
+    
+    
+    #Remove from the joined table
+    cursor.execute("DELETE FROM reddit2.joined WHERE username=%s AND subreddit_name=%s", (username, subreddit_name))
+    
+    return True
+    
+
+    
+
 def signup_case(username="rrreeewewewe", passwd= "223313131"):
     cursor = mysql.connection.cursor()
-    username = "user_name"
-    passwd = "blady"
+    
     
     success = False
     
@@ -25,17 +70,14 @@ def signup_case(username="rrreeewewewe", passwd= "223313131"):
         print("excepion caught")
         pass
     
+    
     if success == 1: #Successfully inserted
         print("Congratulations! Your account has been made")
+        return True
     else:
         print("Error, the account already exists, please choose a different username")
+        return False
         
-    # cursor1 = mysql.connection.cursor()
-    # cursor1.execute('select * from reddit2.users')
-    mysql.connection.commit()
-    
-    # for x in cursor1:
-    #     print(x)
     
 #@app.route("/login.html")
 def login():
@@ -63,9 +105,15 @@ def home():
     return render_template("login.html")
 
 @app.route("/signup.html")
-def signup():
-    signup_case("random name", "random pwd")
+def signup(): 
+    
     return render_template("signup.html")
+
+@app.route("/create-subreddit")
+def create_subreddit():
+    create_subreddit_case()
+    return render_template("signup.html")
+
 
 
 if __name__ == "__main__":
