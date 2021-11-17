62e99e5dc83965e68b23c871650a612e66848da3
taimursalman <58456445+taimursalman@users.noreply.github.com>
2021-11-18 03:06:11 +0500

    backend work 2.0
    some more work done on the backend side
    
    changes made only to taimur salman's functions bhi
    
    I can feel the lack of sleep draining me down into the abyss

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '<]^7f[R2<n'
+app.config['MYSQL_PASSWORD'] = '33e0a108'
 app.config['MYSQL_DB'] = 'reddit2'
 
 mysql = MySQL(app)
 
-def create_subreddit_case(username = "sad", subreddit_name= "woosh", description = "woosh brotha"):
+def create_subreddit_case(username = "saad", subreddit_name= "woosh", description = "woosh brotha"):
     cursor = mysql.connection.cursor()
     
     cursor.execute("Select username from reddit2.users where username=%s", (username,))
@@ -19,49 +19,52 @@ def create_subreddit_case(username = "sad", subreddit_name= "woosh", description
         print("User does not exist")
         return False
     
-    cursor.execute("Select subreddit_name from reddit2.subreddits where subreddit_name=%s", (subreddit_name,))
+    cursor.execute("SELECT name FROM reddit2.subreddits WHERE name=%s", (subreddit_name,))
     if(cursor.fetchone() != None):
         print("Subreddit already exists")
         return False
     
     #Insert into the subreddit table
-    cursor.execute("INSERT INTO reddit2.subreddits VALUES(%s, %s, %s)", (subreddit_name, description, username))
+    cursor.execute("INSERT INTO reddit2.subreddits VALUES(%s, %s)", (subreddit_name, description))
     mysql.connection.commit()
 
     #Insertion into the Joined table, to show that the user has created the subreddit
-    cursor.execute("INSERT INTO reddit2.joined VALUES(%s, %s. %s)", (username, subreddit_name, "Subreddit Owner"))
+    cursor.execute("INSERT INTO reddit2.joined VALUES(%s, %s, %s)", (username, subreddit_name, "Subreddit Owner"))
     mysql.connection.commit()
     
     return True    
     
     
-def leave_subreddit_case(username ="sad", subreddit_name = "rip", ):
+def leave_subreddit_case(username ="saad", subreddit_name = "woosh"):
     cursor = mysql.connection.cursor()
     
     cursor.execute("Select username from reddit2.users where username=%s", (username,))
     if(cursor.fetchone() == None):
         print("User does not exist")
         return False
-    
-    cursor.execute("Select subreddit_name from reddit2.subreddits where subreddit_name=%s", (subreddit_name,))
+
+    cursor.execute("SELECT name FROM reddit2.subreddits WHERE name= %s", (subreddit_name,))
     if(cursor.fetchone() == None):
         print("Subreddit does not exist")
         return False
     
-    
+    #Check if the user is the owner of the subreddit
+    cursor.execute("SELECT roles FROM reddit2.joined WHERE username=%s AND subreddit=%s", (username, subreddit_name))
+    if(cursor.rowcount == 0):
+        return False
+    if(cursor.fetchone()[0] == "Subreddit Owner"):
+        print("You are the owner of this subreddit, you cannot leave :>")
+        return False
     #Remove from the joined table
-    cursor.execute("DELETE FROM reddit2.joined WHERE username=%s AND subreddit_name=%s", (username, subreddit_name))
+    cursor.execute("DELETE FROM reddit2.joined WHERE username=%s AND name=%s", (username, subreddit_name))
     
     return True
     
-
-    
+   
 
 def signup_case(username="rrreeewewewe", passwd= "223313131"):
     cursor = mysql.connection.cursor()
-    
-    
-    success = False
+    success = 0
     
     try:
         success = cursor.execute("INSERT INTO reddit2.users VALUES(%s, %s, %s)", (username, 0, passwd))
@@ -69,7 +72,6 @@ def signup_case(username="rrreeewewewe", passwd= "223313131"):
     except Exception as it_is_what_it_is:
         print("excepion caught")
         pass
-    
     
     if success == 1: #Successfully inserted
         print("Congratulations! Your account has been made")
@@ -101,7 +103,7 @@ def home():
     
     # for x in cursor1:
     #     print(x)
-    login()
+    leave_subreddit_case()
     return render_template("login.html")
 
 @app.route("/signup.html")
