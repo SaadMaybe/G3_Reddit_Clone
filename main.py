79b3ff5037eb145c28059006aeac7b4e09d62e0d
Saad Akbar <88343088+SaadMaybe@users.noreply.github.com>
2021-11-17 22:14:03 +0500

    flask working now with flask-SQL

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'SaadAkbar'
+app.config['MYSQL_DB'] = 'reddit2'
+
+mysql = MySQL(app)
+
 @app.route("/")
 def home():
+    cursor = mysql.connection.cursor()
+    cursor.execute('''SHOW DATABASES''')
+    for x in cursor:
+        print(x)
     return render_template("login.html")
-
-
+    
 @app.route("/signup.html")
 def signup():
     return render_template("signup.html")
