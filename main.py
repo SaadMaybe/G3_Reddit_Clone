14a5a5f5587519a81b1bd47e86274ff6f839c9fe
taimursalman <58456445+taimursalman@users.noreply.github.com>
2021-11-18 01:02:22 +0500

    insertion
    successfully inserting a row into the table using variables

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'SaadAkbar'
+app.config['MYSQL_PASSWORD'] = '33e0a108'
 app.config['MYSQL_DB'] = 'reddit2'
 
 mysql = MySQL(app)
@@ -13,19 +13,29 @@ mysql = MySQL(app)
 @app.route("/")
 def home():
     cursor = mysql.connection.cursor()
-    cursor.execute('''INSERT INTO reddit2.users VALUES('saad', 1234, 0)''')
+    #cursor.execute('''INSERT INTO reddit2.users VALUES('saad', 1234, 0)''')
+    # cursor1 = mysql.connection.cursor()
+    # cursor1.execute('select * from reddit2.users')
+    # mysql.connection.commit()
+    
+    # for x in cursor1:
+    #     print(x)
+    return render_template("login.html")
+
+@app.route("/signup.html")
+def signup():
+    cursor = mysql.connection.cursor()
+    abc = '123'
+    cursor.execute("INSERT INTO reddit2.users VALUES(%s, %s, %s)", (abc, 0, '23'))
+    
     cursor1 = mysql.connection.cursor()
     cursor1.execute('select * from reddit2.users')
     mysql.connection.commit()
     
     for x in cursor1:
         print(x)
-    return render_template("login.html")
-
-@app.route("/signup.html")
-def signup():
-    # sender = request.values.get('username', 'Someone')
-    # print()
+    
+    
     return render_template("signup.html")
 
 
